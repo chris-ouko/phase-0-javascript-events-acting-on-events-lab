0ca27e07a33eda5661eab4d8987ca7d4a3d157f2
+ <+>
2023-09-15 22:01:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const left = dodger.style.left;
+    let newLeft = parseInt(left) + 1;
+  
+    if (newLeft <= window.innerWidth) {
+      dodger.style.left = `${newLeft}px`;
+    }
+  }
+
